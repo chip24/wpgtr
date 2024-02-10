8565bca45a6b22d042f683884f50076c3d6dd297
Chip Morgan <python.chipmorgan@gmail.com>
2024-02-10 08:31:52 -0600

    Add initial file to repo

---
+++ chapter-one.rb
@@ -0,0 +1,13 @@
+['toast','cheese','wine'].each {|food| print food.capitalize }
+
+2.times {print "Yes, I've used chunky bacon in my examples, but never again!"}
+
+##begin
+loop do
+    puts "Much better."
+    puts "Ah,  More space!"
+    puts "My back was killin' me in those crab pincers."
+end
+##end
+
+
